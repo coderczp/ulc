d3e827af87f119424812323e50ebc8c4c9aa3fd0
coder_czp@126.com <coder_czp@126.com>
2017-05-23 21:45:59 +0800

    重构

--- src/test/java/com/czp/ulc/test/TestCompress.java
+++ src/test/java/com/czp/ulc/test/TestCompress.java
@@ -1,23 +1,20 @@
 package com.czp.ulc.test;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 import org.junit.Test;
 
-import com.czp.ulc.common.meta.MemOutStream;
-import com.czp.ulc.common.meta.RACFileReader;
-import com.czp.ulc.common.meta.RACFileWriter;
+import com.github.luben.zstd.ZstdInputStream;
+import com.github.luben.zstd.ZstdOutputStream;
 
 /**
  * @dec Function
@@ -29,80 +26,58 @@ import com.czp.ulc.common.meta.RACFileWriter;
 public class TestCompress {
 
 	@Test
-	public void testRead2() throws Exception {
-		MemOutStream bos = new MemOutStream(15014389);
-		RACFileReader r = new RACFileReader(new File("./test.rac"));
-		r.readBlock(0, bos);
-		r.close();
-		ByteArrayInputStream bis = new ByteArrayInputStream(bos.getBuf(),0,bos.size());
-		 bis.skip(14988072);
-		byte[] x = new byte[195];
-		bis.read(x,0,x.length);
-		System.out.println(new String(x));
-	}
-//	block:0,pos:14987863,size:209
-//	block:0,pos:14988072,size:195
-//	block:0,pos:14988267,size:175
-//	block:0,pos:14988442,size:238
-//	block:0,pos:14988680,size:243
-//	block:0,pos:14988923,size:187
-//	block:0,pos:14989110,size:214
-//	block:0,pos:14989324,size:183
-//	block:0,pos:14989507,size:182
-
-	@Test
-	public void testWrite2() throws IOException {
-		String name = "./test.rac";
-		File file = new File("./2.log");
-		File f = new File(name);
-		FileOutputStream out = new FileOutputStream(f);
-		BufferedReader lines = Files.newBufferedReader(file.toPath());
-		String line = null;
-		RACFileWriter rout = new RACFileWriter(out);
-		int pos = 0, len = 0, bPos = 0;
-		while ((line = lines.readLine()) != null) {
-			byte[] bytes = line.getBytes();
-			int blockPos = rout.writeAndReturnBlock(bytes);
-			if (bPos != blockPos) {
-				pos = 0;
-				bPos = blockPos;
-			}
-			len = bytes.length;
-			String format = String.format("block:%s,pos:%s,size:%s", blockPos, pos, len);
-			System.out.println(format);
-			pos += len;
-		}
-		System.out.println(file.length());
-		rout.closeAndReturnBlock();
-		lines.close();
-		out.close();
-	}
-
-	@Test
-	public void testWrite() throws IOException {
+	public void testGzipWrite() throws IOException {
+		long st = System.currentTimeMillis();
 		GZIPOutputStream gzos = new GZIPOutputStream(new FileOutputStream("./test.gz"));
-		BufferedReader lines = Files.newBufferedReader(new File("./3.log").toPath());
-		long offset = 0, i = 0;
+		BufferedReader lines = Files.newBufferedReader(new File("./9.log").toPath());
 		String line = null;
 		while ((line = lines.readLine()) != null) {
-			line += "\n";
 			byte[] bytes = line.getBytes();
-			offset += bytes.length;
 			gzos.write(bytes);
-			System.out.println(i + "--->" + offset);
-			i++;
 		}
 		lines.close();
 		gzos.close();
+		System.out.println(System.currentTimeMillis() - st);
 	}
 
 	@Test
-	public void testRead() throws FileNotFoundException, IOException {
+	public void testZstdWrite() throws IOException {
+		long st = System.currentTimeMillis();
+		ZstdOutputStream gzos = new ZstdOutputStream(new FileOutputStream("./test.zs"));
+		BufferedReader lines = Files.newBufferedReader(new File("./9.log").toPath());
+		String line = null;
+		while ((line = lines.readLine()) != null) {
+			byte[] bytes = line.getBytes();
+			gzos.write(bytes);
+		}
+		lines.close();
+		gzos.close();
+		System.out.println(System.currentTimeMillis() - st);
+	}
+
+	@Test
+	public void testGzipRead() throws FileNotFoundException, IOException {
 		GZIPInputStream gz = new GZIPInputStream(new FileInputStream("./test.gz"));
-		gz.skip(141845765);
-		BufferedReader br = new BufferedReader(new InputStreamReader(gz));
-		System.out.println(br.readLine());
-		br.close();
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("./gz.log"));
+		byte[] buf = new byte[1024];
+		int n = -1;
+		while ((n = gz.read(buf)) != -1) {
+			bos.write(buf, 0, n);
+		}
+		bos.close();
+		gz.close();
+	}
+
+	@Test
+	public void testZstdRead() throws FileNotFoundException, IOException {
+		ZstdInputStream gz = new ZstdInputStream(new FileInputStream("./test.zs"));
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("./zs.log"));
+		byte[] buf = new byte[1024];
+		int n = -1;
+		while ((n = gz.read(buf)) != -1) {
+			bos.write(buf, 0, n);
+		}
+		bos.close();
 		gz.close();
 	}
 
